6179d9327ba23d9e545caf4bb7eeb8d9d8c97600
juliamorokina <juliamorokina@mail.ru>
2023-01-12 22:39:59 +0300

    create controllers

--- src/product/product.model.ts
+++ src/product/product.model.ts
@@ -1,4 +1,5 @@
 export class ProductModel {
+  _id: string;
   advantages: string;
   calculatedRaiting: number;
   characteristics: {
--- src/review/review.model.ts
+++ src/review/review.model.ts
@@ -1,4 +1,5 @@
 export class ReviewModel {
+  _id: string;
   createdAt: Date;
   description: string;
   name: string;
