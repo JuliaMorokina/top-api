70b46ce5948700c2faa2f9e71351388c1941737c
juliamorokina <juliamorokina@mail.ru>
2023-03-12 22:08:06 +0300

    module for upload files

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { ReviewModule } from './review/review.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { getMongoConfig } from './configs/mogo.config';
+import { FilesModule } from './files/files.module';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { getMongoConfig } from './configs/mogo.config';
     TopPageModule,
     ProductModule,
     ReviewModule,
+    FilesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
