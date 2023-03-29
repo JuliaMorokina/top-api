afd90a35e56416a7e0bb9f6a7dc3b9aac73d8944
juliamorokina <juliamorokina@mail.ru>
2023-03-29 22:03:41 +0300

    telegram bot for review

---
+++ src/configs/telegram.config.ts
@@ -0,0 +1,17 @@
+import { ConfigService } from '@nestjs/config';
+import { ITelegramOptions } from 'src/telegram/telegram.inteface';
+
+export const getTelegramConfig = (
+  configService: ConfigService,
+): ITelegramOptions => {
+  const token = configService.get('TELEGRAM_TOKEN');
+
+  if (!token) {
+    throw new Error('TELEGRAM TOKEN не задан');
+  }
+
+  return {
+    chatId: configService.get('CHAT_ID') ?? '',
+    token,
+  };
+};
--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypegooseModule } from 'nestjs-typegoose';
+import { TelegramModule } from 'src/telegram/telegram.module';
 import { ReviewController } from './review.controller';
 import { ReviewModel } from './review.model';
 import { ReviewService } from './review.service';
@@ -10,6 +11,7 @@ import { ReviewService } from './review.service';
     TypegooseModule.forFeature([
       { typegooseClass: ReviewModel, schemaOptions: { collection: 'Review' } },
     ]),
+    TelegramModule,
   ],
   providers: [ReviewService],
 })
